22dc936224cbb65f0933493141d08e0529ff80c8
Siddhapura-Yash <siddhapurayash09@gmail.com>
2025-06-28 18:09:44 +0530

    Create CMP.v

---
+++ CMP.v
@@ -0,0 +1,9 @@
+module CMP(data0,data1,lt,gt,eq);
+  input [15:0]data0,data1;
+  output reg lt,gt,eq;
+  
+  assign lt = data0 < data1;
+  assign gt = data0 > data1;
+  assign eq = data0 == data1;
+  
+endmodule
